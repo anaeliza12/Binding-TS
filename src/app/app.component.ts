359cf0f27d16ea9ab7772c2d2566e3e69e03f501
Ana Eliza <anaelizaperobelli@gmail.com>
2024-05-21 23:56:09 -0300

    TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+
+  check(event: any) {
+    var verification = true;
+    if (event == false) {
+      verification = false;
+    }
+    return verification;
+
+  }
 }
